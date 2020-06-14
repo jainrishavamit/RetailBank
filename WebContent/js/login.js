f4141be85893153054323936ee3ae353024a3852
Jain Rishav Amit <jainrishavamit1@gmail.com>
2020-06-14 16:30:44 +0530

    User story 1 UI validations done

--- WebContent/js/login.js
+++ WebContent/js/login.js
@@ -1,4 +1,5 @@
 function checkFields(){
+	var password=document.getElementById("Password").value;
 	if(document.getElementById("Userid").value.length<8 ){
 		alert("Invalid User Id :\n User ID should be of atleast 8 characters");
 		return false;
@@ -8,10 +9,31 @@ function checkFields(){
 		alert("Invalid User Id :\nUser ID should be of atmost 20 characters");
 		return false;
 	}
-	else if(document.getElementById("Password").value.length){
-		
+	else if(password.length<10){
+		alert("Invalid Password :\n Password should be of atleast 10 character\n Password should be of atmost 20 characters \n Password should contain atleast one upper case" +
+		"\n Password should contain atleast one numeric digit\n Password should contain atleast one special character from (@#$%^&*)");
+	return false;
 	}
-	
-	alert("everything fine");
+	else if(password.length>20){
+		alert("Invalid Password :\n Password should be of atleast 10 character\n Password should be of atmost 20 characters \n Password should contain atleast one upper case" +
+				"\n Password should contain atleast one numeric digit\n Password should contain atleast one special character from (@#$%^&*)");
+		return false;
+	}
+	else if(!(/[A-Z]+/.test(password))){
+		alert("Invalid Password :\n Password should be of atleast 10 character\n Password should be of atmost 20 characters \n Password should contain atleast one upper case" +
+			"\n Password should contain atleast one numeric digit\n Password should contain atleast one special character from (@#$%^&*)");
+		return false;
+	}
+	else if(!(/[0-9]+/.test(password))){
+		alert("Invalid Password :\n Password should be of atleast 10 character\n Password should be of atmost 20 characters \n Password should contain atleast one upper case" +
+			"\n Password should contain atleast one numeric digit\n Password should contain atleast one special character from (@#$%^&*)");
+		return false;
+	}
+	else if(!(/[@#$%^&*]+/.test(password))){
+		alert("Invalid Password :\n Password should be of atleast 10 character\n Password should be of atmost 20 characters \n Password should contain atleast one upper case" +
+			"\n Password should contain atleast one numeric digit\n Password should contain atleast one special character from (@#$%^&*)");
+		return false;
+	}
+	//alert("everything fine");
 	return true;
 }
